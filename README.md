4cdbd616a06383147043f08124b888830e85f558
Greg Martyn <greg.martyn@gmail.com>
2023-12-13 14:05:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Converts a string to a boolean, treating '0', 'false', 'FALSE', 'off' and falsy values like '' to false, the rest to true.
---
+++ esm/StringToBoolean.mjs
@@ -0,0 +1,13 @@
+/**
+ * Converts a string to a boolean, treating '0', 'false', 'FALSE', 'off' and falsy values like '' to false, the rest to true.
+ * @param {string|boolean} value
+ */
+const stringToBoolean = (value) => typeof value === 'boolean'
+	? value // param is already boolean
+	: typeof value === 'string'
+		? [ '0', 'false', 'off' ].includes(value.toLowerCase())
+			? false
+			: Boolean(value) // returns false for ''; true for non-empty string
+		: Boolean(value); // returns false for null or undefined, true for objects
+
+export default stringToBoolean;
